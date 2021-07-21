7692375b619e64ec92cd2a62601c84d9b5cdbebe
Dung <dunggamo2000@gmail.com>
2021-07-21 10:58:14 +0700

    Organize code v2 (css)

--- PROJECT/js/main.js
+++ PROJECT/js/main.js
@@ -4,28 +4,23 @@ var tableFilterDepartment = document.querySelector(".table-filter__department");
 var tableFilterDepartmentList = document.querySelector(".table-filter__department .department-list");
 var tableFilterPosition = document.querySelector(".table-filter__position");
 var tableFilterPositionList = document.querySelector(".table-filter__position .department-list");
-var popupCancel = document.querySelector(".popup-cancel");
-var popupSave = document.querySelector(".popup-save");
 
-
-
-
-
-
+// Fpcus vào ô search thì thêm border vào
 tableSearchInput.addEventListener("focus", () => {
     tableSearch.style.border = "1px solid #019160";
 });
 
-
+// Blur ô search thì về border cũ
 tableSearchInput.addEventListener("blur", () => {
     tableSearch.style.border = "1px solid #bbb";
 });
 
-
+// Ẩn hiện lọc phòng ban
 tableFilterDepartment.addEventListener("click", () => {
     tableFilterDepartment.classList.toggle("show");
 });
 
+// Ẩn hiện lọc vị trí
 tableFilterPosition.addEventListener("click", () => {
     tableFilterPosition.classList.toggle("show");
 });
