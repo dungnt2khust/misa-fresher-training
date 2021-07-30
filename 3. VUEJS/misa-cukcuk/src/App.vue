27e3569b5f83569473fefcd336f7f53cf7a4a4d1
Dung <dunggamo2000@gmail.com>
2021-07-30 23:11:14 +0700

    reorganize components

--- 3. VUEJS/misa-cukcuk/src/App.vue
+++ 3. VUEJS/misa-cukcuk/src/App.vue
@@ -1,68 +1,33 @@
 <template>
   <div id="app"> 
-    <TheMain 
-      :employees="employees"
-      />
-    <ThePopupInfo
-      :popupData="popupData"
-      :popupShow="popupShow"
-
-      @popupCancelClick="popupCancelClick()"
-      />
-    <BaseToastMessage
-
-    />
+    <div id="main">
+      <EmployeeHeader/>
+      <div id="wrapper">
+        <EmployeeMenu/>
+        <EmployeePage/>
+      </div>
+    </div>
+    <ThePopupInfo/>
+    <EmployeeToast/>
   </div>
 </template>
 
 <script>
-import TheMain from './components/layout/TheMain.vue'
+import TheHeader from './components/layout/TheHeader.vue'
+import TheMenu from './components/layout/TheMenu.vue'
+import EmployeePage from './view/employee/EmployeePage.vue'
 import ThePopupInfo from './components/layout/ThePopupInfo.vue'
 import BaseToastMessage from './components/base/BaseToastMessage.vue'
-import axios from 'axios'
-import {EventBus} from './main.js'
 
 export default {
 
   name: 'App',
-  data() {
-      return {
-          employees: [],
-          popupShow: false,
-          popupData: {}
-      }
-  },
-  methods: {
-    employeeRowClick(event) {
-      this.popupData = this.employees[event];
-      this.popupShow = true;
-    },
-    popupCancelClick() {
-      this.popupShow = false;
-    }
-  },
-  created() {
-      const vm = this;
-      axios.get('http://cukcuk.manhnv.net/v1/Employees').then(res => {
-          vm.employees = res.data;
-      }).catch(res => {
-          console.log(res.data);
-      });
-
-      // Lắng nghe sự kiện (EVENTBUS)
-      EventBus.$on('employeeRowClick', (data) => {
-        this.employeeRowClick(data);
-      }); 
-  },
-  destroyed() { 
-      EventBus.$off('employeeRowClick', (data) => {
-        this.employeeRowClick(data);
-      });
-  },
   components: {
-    TheMain,
+    EmployeeHeader: TheHeader,
+    EmployeeMenu: TheMenu,
+    EmployeePage,
     ThePopupInfo,
-    BaseToastMessage
+    EmployeeToast: BaseToastMessage
   }
 }
 </script>
---
+++ 3. VUEJS/misa-cukcuk/src/components/layout/TheControl.vue
@@ -0,0 +1,77 @@
+<template lang="">
+	<div id="container__control">
+		<div class="container__header-left">
+			<span class="table-name">Danh sách nhân viên</span>
+			<div class="table-filter">
+				<div class="table-search">
+					<div class="table-search__icon">
+						<img src="../../assets/icon/search.png" alt="" />
+					</div>
+					<input
+						tabindex="1"
+						class="table-search__input"
+						placeholder="Tìm theo mã, họ tên hoặc số điện thoại"
+						type="text"
+					/>
+				</div>
+				<!-- DEPARTMENT -->
+				<BaseDropdownFilter
+					:id="'filter-department'"
+					:class="{ 'table-filter__department': true }"
+					:tabindex="2"
+					:APIurl="APIurl__DEPARTMENT"
+					:dropdownDefaultVal="dropdownDefaultVal__DEPARTMENT"
+					:dropdownName="dropdownName__DEPARTMENT"
+				/>
+				<!-- POSITION -->
+				<BaseDropdownFilter
+					:id="'filter-position'"
+					:class="{ 'table-filter__position': true }"
+					:tabindex="3"
+					:APIurl="APIurl__POSITION"
+					:dropdownDefaultVal="dropdownDefaultVal__POSITION"
+					:dropdownName="dropdownName__POSITION"
+				/>
+			</div>
+		</div>
+		<div class="container__header-right">
+			<button class="button button-addemployee">
+				<div class="button__img">
+					<img src="../../assets/icon/add.png" alt="" />
+				</div>
+				<span class="button__name"> Thêm nhân viên </span>
+			</button>
+			<div class="button-f5-delete">
+				<button id="button-delete" class="button">
+					<i class="fas fa-minus-square"></i> Xoá nhân viên
+				</button>
+				<div class="refresh">
+					<img src="../../assets/icon/refresh.png" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	import BaseDropdownFilter from "../base/BaseDropdownFilter.vue";
+
+	export default {
+		name: "TheControl",
+		data() {
+			return {
+				// DEPARTMENT
+				APIurl__DEPARTMENT: "http://cukcuk.manhnv.net/api/Department",
+				dropdownDefaultVal__DEPARTMENT: "Tất cả phòng ban",
+				dropdownName__DEPARTMENT: "Department",
+				// POSITION
+				APIurl__POSITION: "http://cukcuk.manhnv.net/v1/Positions",
+				dropdownDefaultVal__POSITION: "Tất cả vị trí",
+				dropdownName__POSITION: "Position",
+			};
+		},
+		components: {
+			BaseDropdownFilter,
+		},
+	};
+</script>
+<style lang=""></style>
