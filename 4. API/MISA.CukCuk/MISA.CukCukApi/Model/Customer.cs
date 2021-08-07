9dc97ff6bea7d7710ad623fed1f9f94a967c4948
Dung <dunggamo2000@gmail.com>
2021-08-07 10:01:41 +0700

    API .net Core (GET, POST, PUT, DEL) customer

---
+++ 4. API/MISA.CukCuk/MISA.CukCukApi/Model/Customer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.CukCukApi.Model
+{
+    public class Customer
+    {
+        #region Property
+        /// <summary>
+        /// Khoá chính
+        /// </summary>
+        public Guid CustomerId { get; set; }
+        /// <summary>
+        /// Mã khách hàng
+        /// </summary>
+        public string CustomerCode { get; set; }
+        /// <summary>
+        /// Họ
+        /// </summary>
+        public string FirstName  { get; set; }
+        /// <summary>
+        /// Tên đệm và tên
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Tên đầy đủ
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Giới tính
+        /// </summary>
+        public int? Gender { get; set; }
+        /// <summary>
+        /// Địa chỉ
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Ngày tháng năm sinh
+        /// </summary>
+        public DateTime? DateOfBirth { get; set; }
+        /// <summary>
+        /// Địa chỉ Email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Số điện thoại
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Khoá ngoại tham chiếu đến bảng customerGroup
+        /// </summary>
+        public Guid CustomerGroupId { get; set; }
+        /// <summary>
+        /// Số nợ
+        /// </summary>
+        public int? DebitAmount { get; set; }
+
+        /// <summary>
+        /// Mã thẻ thành viên
+        /// </summary>
+        public string MemberCardCode { get; set; }
+        /// <summary>
+        /// Tên công ty
+        /// </summary>
+        public string CompanyName { get; set; }
+        /// <summary>
+        /// Mã số thuế công ty
+        /// </summary>
+        public string CompanyTaxCode { get; set; }
+        /// <summary>
+        /// Trạng thái theo dõi
+        /// </summary>
+        public int? IsStopFollow { get; set; }
+        /// <summary>
+        /// Ngày tạo
+        /// </summary>
+        public DateTime? CreatedDate { get; set; }
+        /// <summary>
+        /// Được tạo bởi
+        /// </summary>
+        public string CreatedBy { get; set; }
+        /// <summary>
+        /// Ngày chỉnh sửa
+        /// </summary>
+        public DateTime? ModifiedDate { get; set; }
+        /// <summary>
+        /// Được chỉnh sửa bởi
+        /// </summary>
+        public string ModifiedBy { get; set; }
+        #endregion
+    }
+}
